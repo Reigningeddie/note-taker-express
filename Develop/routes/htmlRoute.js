f1d6d85613d1f9464b8fe8382fdf1f9ad76c4467
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-06-18 23:51:46 -0700

    worked on bugs

--- Develop/routes/htmlRoute.js
+++ Develop/routes/htmlRoute.js
@@ -1,11 +1,12 @@
-var router = require("express").Router();
-var path = require("path");
+var router =require("express").Router();
+var path =require("path");
+
 
 router.get("/notes", function(req, res){
-    res.sendFiles(path.join(__dirname, "../public/notes.html"))
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
 })
 router.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname,"../public/index.html"))
 })
 
-module.exports = router 
+module.exports = router
